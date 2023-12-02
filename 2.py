1598d21dcc7aba3d2260efd06cafe3356f1ea5ee
Svyatoslav Usachev <kaathewise@gmail.com>
2023-12-02 20:43:13 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,6 +6,6 @@ from math import prod
 from operator import or_
 from re import findall
 
-#print(sum(i+1 for i, s in enumerate(open(0)) if all(dict(red=12,green=13,blue=14)[w]>=int(n) for n,w in findall(r'(\d+) (\w+)',s))))
+#print(sum(i+1 for i,s in enumerate(open(0)) if all('rgb'.find(w)+12>=int(n) for n,w in findall(r'(\d+) (\w)',s))))
 
-print(sum(prod(reduce(or_,(Counter({w:int(n)}) for n,w in findall(r'(\d+) (\w+)',s))).values()) for s in open(0)))
+print(sum(prod(reduce(or_,(Counter({w:int(n)}) for n,w in findall(r'(\d+) (\w)',s))).values()) for s in open(0)))
