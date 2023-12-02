e59349e4295b4c4a37dd2f8e450ab4eddd1095f6
Svyatoslav Usachev <kaathewise@gmail.com>
2023-12-02 10:14:56 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,6 @@
 from functools import reduce
 from re import findall, search
-from sys import stdin
 
-#print(sum((d:=findall(r'\d', s)) and int(d[0]+d[-1]) for s in stdin.readlines()))
+#print(sum((d:=findall(r'\d', s)) and int(d[0]+d[-1]) for s in open(0)))
 
-print(sum((r:='one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9') and int(''.join(str(r.split('|').index(search(x+'(%s)'%r,s)[1])%9+1) for x in ['', '.*'])) for s in stdin.readlines()))
+print(sum((r:='one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9') and int(''.join(str(r.split('|').index(search(x+'(%s)'%r,s)[1])%9+1) for x in ['', '.*'])) for s in open(0)))
