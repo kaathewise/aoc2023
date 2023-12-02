092f7251ecd28264986bba58db8816d0ab9c424d
Svyatoslav Usachev <kaathewise@gmail.com>
2023-12-02 10:19:09 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,6 +4,6 @@ from math import prod
 from operator import or_
 from re import findall
 
-#print(sum(i+1 for i, s in enumerate(open(0)) if all(Counter(dict((x[1],int(x[0])) for x in findall(r'(\d+) (\w+)',c)))<=Counter(red=12,green=13,blue=14) for c in s.split(';'))))
+#print(sum(i+1 for i, s in enumerate(open(0)) if all(Counter({x[1]:int(x[0]) for x in findall(r'(\d+) (\w+)',c)})<=Counter(red=12,green=13,blue=14) for c in s.split(';'))))
 
-print(sum(prod(reduce(or_,(Counter(dict((x[1],int(x[0])) for x in findall(r'(\d+) (\w+)',c))) for c in s.split(';'))).values()) for s in open(0)))
+print(sum(prod(reduce(or_,(Counter({x[1]:int(x[0]) for x in findall(r'(\d+) (\w+)',c)}) for c in s.split(';'))).values()) for s in open(0)))
