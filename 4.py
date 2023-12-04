63ca91a861f6e042b7c18ac47d4f61eecf1a806e
Svyatoslav Usachev <kaathewise@gmail.com>
2023-12-04 11:34:24 +0000

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,6 @@
 from functools import reduce
-from itertools import repeat, zip_longest
+from itertools import chain, repeat
 
 #print(sum((1<<len(s)-len({*s}))//2 for s in map(str.split,open(0))))
 
-print(reduce((lambda r,x:(c:=next(r[1])) and (r[0]+c,map(sum,zip_longest(r[1],repeat(c,x),fillvalue=0)))), (len(s)-len({*s}) for s in map(str.split,open(0))), (0,repeat(1)))[0])
+print(reduce((lambda r,x:(c:=next(r[1])) and (r[0]+c,chain(map(sum,zip(repeat(c,x),r[1])),r[1]))), (len(s)-len({*s}) for s in map(str.split,open(0))), (0,repeat(1)))[0])
