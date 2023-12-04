9b93d146ae245b8e1eab7177ac3893e9e6ed41e5
Svyatoslav Usachev <kaathewise@gmail.com>
2023-12-04 11:50:43 +0000

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,7 @@
 from functools import reduce
 from itertools import chain, repeat
+from operator import add
 
 #print(sum((1<<len(s)-len({*s}))//2 for s in map(str.split,open(0))))
 
-print(reduce((lambda r,x:(c:=next(r[1])) and (r[0]+c,chain(map(sum,zip(repeat(c,x),r[1])),r[1]))), (len(s)-len({*s}) for s in map(str.split,open(0))), (0,repeat(1)))[0])
+print(reduce((lambda r,x:(c:=next(r[1])) and (r[0]+c,chain(map(add,repeat(c,x),r[1]),r[1]))), (len(s)-len({*s}) for s in map(str.split,open(0))), (0,repeat(1)))[0])
