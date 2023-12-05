5052c70eaaef3945106148949ec0b3401f373e5b
Svyatoslav Usachev <kaathewise@gmail.com>
2023-12-05 16:41:21 +0000

    Update 5.py

--- 5.py
+++ 5.py
@@ -2,6 +2,6 @@ from functools import reduce
 from itertools import chain
 from re import findall
 
-#(f:=open(0).read().split('\n\n')) and print(min(reduce((lambda s,m:[(z:=map(int,findall(r'\d+',m))) and next((u+x-v for u,v,w in zip(z,z,z) if v<=x<v+w),x) for x in s]),f[1:],map(int,findall(r'\d+',f[0])))))
+#(f:=open(0).read().split('\n\n')) and print(min(reduce((lambda s,m:[next((u+x-v for u,v,w in zip(*[map(int,findall(r'\d+',m))]*3) if v<=x<v+w),x) for x in s]),f[1:],map(int,findall(r'\d+',f[0])))))
 
-(f:=open(0).read().split('\n\n')) and print(min(reduce((lambda s,m:(z:=map(int,findall(r'\d+',m))) and (p:=[*zip(z,z,z)]) and [next(((u+x-v,l) for u,v,w in p if v<=x and x+l<=v+w),(x,l)) for x,l in reduce((lambda r,a:{*chain(*([(x,a-x),(a,x+l-a)] if x<a<x+l else [(x,l)] for x,l in r))}),chain(*((v,v+w) for _,v,w in p)),s)]),f[1:],(i:=map(int,findall(r'\d+',f[0]))) and [*zip(i,i)]))[0])
+(f:=open(0).read().split('\n\n')) and print(min(reduce((lambda s,m:(p:=[*zip(*[map(int,findall(r'\d+',m))]*3)]) and [next(((u+x-v,l) for u,v,w in p if v<=x<v+w),(x,l)) for x,l in reduce((lambda r,a:{*chain(*([(x,a-x),(a,x+l-a)] if x<a<x+l else [(x,l)] for x,l in r))}),chain(*((v,v+w) for _,v,w in p)),s)]),f[1:],zip(*[map(int,findall(r'\d+',f[0]))]*2)))[0])
