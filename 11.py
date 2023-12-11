92e8d1a6ab125aa3ec2c8df6e5c1908f17dab383
Svyatoslav Usachev <kaathewise@gmail.com>
2023-12-11 11:36:53 +0000

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,6 +1,6 @@
 from itertools import count
 from math import sumprod
 
-#(m:=[(i,j) for i,s in enumerate(open(0)) for j,c in enumerate(s) if c=='#']) and print(sum((a:=sorted(x[i] for x in m)) and (c:=dict(zip(sorted({*a}),count()))) and sumprod((2*x-c[x] for x in a),range(-len(m)+1,len(m),2)) for i in [0,1]))
+#print(sum((c:=dict(zip(sorted({*a}),count()))) and sumprod((2*x-c[x] for x in sorted(a)),range(-len(a)+1,len(a),2)) for a in zip(*((i,j) for i,s in enumerate(open(0)) for j,c in enumerate(s) if c=='#'))))
 
-(m:=[(i,j) for i,s in enumerate(open(0)) for j,c in enumerate(s) if c=='#']) and print(sum((a:=sorted(x[i] for x in m)) and (c:=dict(zip(sorted({*a}),count()))) and sumprod((x+999999*(x-c[x]) for x in a),range(-len(m)+1,len(m),2)) for i in [0,1]))
+print(sum((c:=dict(zip(sorted({*a}),count()))) and sumprod((x+999999*(x-c[x]) for x in sorted(a)),range(-len(a)+1,len(a),2)) for a in zip(*((i,j) for i,s in enumerate(open(0)) for j,c in enumerate(s) if c=='#'))))
