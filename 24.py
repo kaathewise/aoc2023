92e4f08d1a56ff13b9bf03dd54fd2f88d81b0e66
Svyatoslav Usachev <kaathewise@gmail.com>
2023-12-24 14:52:09 +0000

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,3 +1,3 @@
 from re import findall
 
-(m:=[(x+1j*y,u+1j*v,2j*v) for s in open(0) for x,y,z,u,v,_ in [map(int,findall(r'-?\d+',s))]]) and print(sum((d:=-(u*(w:=v-r)).imag)!=0 and (t:=((x-y)*w).imag/d)>0 and ((x-y)*(u-q)).imag/d>0 and all(2e14<=((x+t*u)/p).real<=4e14 for p in [1,1j]) for x,u,q in m for y,v,r in m)//2)
+(m:=[(x+1j*y,u+1j*v,2j*v) for s in open(0) for x,y,z,u,v,_ in [map(int,findall(r'-?\d+',s))]]) and print(sum((d:=-(u*(w:=v-r)).imag) and (t:=((x-y)*w).imag/d)>0 and ((x-y)*(u-q)).imag/d>0 and all(2e14<=((x+t*u)/p).real<=4e14 for p in [1,1j]) for x,u,q in m for y,v,r in m)//2)
